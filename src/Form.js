885dc6ea393d63b160a892a8499eabeffdef1750
Amsel1676 <kuro.amsel@gmail.com>
2020-05-04 15:32:10 +0900

    Place a temporary button

--- src/Form.js
+++ src/Form.js
@@ -1,12 +1,21 @@
 import React from 'react'
 
-function Form() {
-    return (
-        <React.Fragment>
-            <label>todo:</label>
-            <input type="text" className="todoform" />
-        </React.Fragment>
-    );
+class Form extends React.Component {
+    handleClick = () => {
+        console.log("Click");
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <label>todo:</label>
+                <input type="text" className="todoform" />
+                <button onClick={() => this.handleClick()}>
+                  Click me
+                </button>
+            </React.Fragment>
+        );
+    }
 }
 
 export default Form;
