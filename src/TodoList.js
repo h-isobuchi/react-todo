251985f259593315bebd7986c9fc8575a2c80a60
Amsel1676 <kuro.amsel@gmail.com>
2020-05-05 16:21:59 +0900

    Place Temporary checkbox

--- src/TodoList.js
+++ src/TodoList.js
@@ -4,9 +4,9 @@ class TodoList extends React.Component {
     render() {
         return (
             <ul>
-                <li>Item 1 <input type="submit" value="delete" /></li>
-                <li>Item 2 <input type="submit" value="delete" /></li>
-                <li>Item 3 <input type="submit" value="delete" /></li>
+                <li><input type="checkbox" /> Item 1 <input type="submit" value="delete" /></li>
+                <li><input type="checkbox" /> Item 2 <input type="submit" value="delete" /></li>
+                <li><input type="checkbox" /> Item 3 <input type="submit" value="delete" /></li>
             </ul>
         );
     }
