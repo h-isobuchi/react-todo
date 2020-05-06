d02ef3170bd97ddf6c3674095667c0fb1cbeb19d
Amsel1676 <kuro.amsel@gmail.com>
2020-05-06 18:00:07 +0900

    Transfer state

--- src/App.js
+++ src/App.js
@@ -2,14 +2,27 @@ import React from 'react';
 import Form from './Form';
 import TodoList from './TodoList'
 
-function App() {
-  return (
-    <React.Fragment>
-      <h1>Todo App</h1>
-      <Form/>
-      <TodoList/>
-    </React.Fragment>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {value: ''}
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value})
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Todo App</h1>
+        <Form value={this.value} handleChange={this.handleChange} />
+        <TodoList/>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- src/Form.js
+++ src/Form.js
@@ -1,23 +1,12 @@
 import React from 'react'
 
 class Form extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {value: ''}
-
-        this.handleChange = this.handleChange.bind(this)
-    }
-
-    handleChange(event) {
-        this.setState({value: event.target.value})
-    }
-
     render() {
         return (
             <React.Fragment>
                 <label>
                     todo:
-                    <input type="text" className="todoform" value={this.state.value} onChange={this.handleChange} />
+                    <input type="text" className="todoform" value={this.props.value} onChange={this.props.handleChange} />
                     <input type="submit" value="Add" />
                 </label>
             </React.Fragment>
