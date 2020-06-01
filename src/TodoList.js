f61759332a6739d173d555acb33010ef352aa7a8
Amsel1676 <kuro.amsel@gmail.com>
2020-06-02 00:42:21 +0900

    Apply 'material-ui'

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Button from '@material-ui/core/Button';
 
 class TodoList extends React.Component {
     render() {
         return (
             <ul>
                {this.props.todos.map(
-                   (todo, i) => <li key={i}><input type="checkbox" />{todo}<button onClick={() => this.props.handleDeleteClick(i)}>Delete</button>
+                   (todo, i) => <li key={i}><input type="checkbox" />{todo}<Button variant="contained" color="primary" onClick={() => this.props.handleDeleteClick(i)}>Delete</Button>
                    </li>
                )}
             </ul>
