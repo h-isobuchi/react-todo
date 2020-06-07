3d3363a417600876ce3ad016d74cc18c48bd1d08
Amsel1676 <kuro.amsel@gmail.com>
2020-06-07 15:08:55 +0900

    Change delete button icon

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/core/styles';
 import { green } from '@material-ui/core/colors';
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
 import './TodoList.css'
 
 const GreenCheckbox = withStyles({
@@ -26,7 +27,9 @@ class TodoList extends React.Component {
                             control={<GreenCheckbox checked={this.props.todos[i].completed} onChange={() => this.props.handleCheckChange(i)} name="checkedG" />}
                         />
                         <span className={this.props.todos[i].completed ? "text-line-through" : "plain-text"}>{this.props.todos[i].todo}</span>
-                        <Button variant="contained" color="primary" onClick={() => this.props.handleDeleteClick(i)}>Delete</Button>
+                        <IconButton aria-label="delete" onClick={() => this.props.handleDeleteClick(i)}>
+                          <DeleteIcon />
+                        </IconButton>
                    </li>
                )}
             </ul>
