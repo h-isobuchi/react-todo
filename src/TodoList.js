a4028f8e620d46ea0eca0bfc49d0d99bbcde3c96
Amsel1676 <kuro.amsel@gmail.com>
2020-06-14 20:11:46 +0900

    Apply List component og material ui

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
 import { green } from '@material-ui/core/colors';
 import Checkbox from '@material-ui/core/Checkbox';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
 import './TodoList.css'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
 
 const GreenCheckbox = withStyles({
     root: {
@@ -17,24 +29,35 @@ const GreenCheckbox = withStyles({
     checked: {},
   })((props) => <Checkbox color="default" {...props} />);
 
-class TodoList extends React.Component {
-    render() {
-        return (
-            <ul>
-               {this.props.todos.map(
-                   (todolist, i) => <li key={i}>
-                       <FormControlLabel
-                            control={<GreenCheckbox checked={todolist.completed} onChange={() => this.props.handleCheckChange(i)} name="checkedG" />}
-                        />
-                        <span className={todolist.completed ? "text-line-through" : "plain-text"}>{todolist.todo}</span>
-                        <IconButton aria-label="delete" onClick={() => this.props.handleDeleteClick(i)}>
-                          <DeleteIcon />
-                        </IconButton>
-                   </li>
-               )}
-            </ul>
-        );
-    }
+function TodoList(props) {
+  const classes = useStyles();
+
+  return (
+    <List className={classes.root}>
+      {props.todos.map(
+        (todolist, i) => {
+          const labelId = `checkbox-list-label-${i}`;
+
+          return (
+            <ListItem key={i}>
+              <ListItemIcon>
+                <GreenCheckbox 
+                  checked={todolist.completed}
+                  onChange={() => props.handleCheckChange(i)}
+                  name="checkedG" 
+                />
+                </ListItemIcon>
+                <ListItemText id={labelId} className={todolist.completed ? "text-line-through" : "plain-text"} primary={todolist.todo}/>                      
+                <ListItemSecondaryAction>
+                  <IconButton aria-label="delete" onClick={() => props.handleDeleteClick(i)}>
+                    <DeleteIcon />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>
+              )
+            })}
+    </List>
+  );
 }
 
 export default TodoList;
